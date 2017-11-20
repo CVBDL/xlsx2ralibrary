1a8f98d28fc67c202b4dd19977a84f834d80f012
zhongsp <patrick.zhongsp@gmail.com>
2017-11-20 11:07:32 +0800

    support logging

--- README.md
+++ README.md
@@ -2,10 +2,26 @@
 
 RaLibrary command line import tool.
 
+> The input file must be closed when running this tool.
+> Because we will write logs to this file.
+
+## Examples
+
 ```sh
 # show help
 xlsx2ralibrary.py --help
 
 # import data
-xlsx2ralibrary.py --user-name=username --password=pwd --path=books.xlsx
+python3 xlsx2ralibrary --user-name=username --password=pwd --path=./xlsx2ralibrary/books.xlsx
+```
+
+## Input excel file format
+
+The first row should be a header row.
+And this row will be ignored when creating books.
+
+```txt
+| ISBN          | Code  | Book Name            |
+| ------------- | ----- | -------------------- |
+| 9780596008031 | P501  | Designing Interfaces |
 ```
--- xlsx2ralibrary/xlsx2ralibrary/xlsx2ralibrary.py
+++ xlsx2ralibrary/xlsx2ralibrary/xlsx2ralibrary.py
@@ -1,8 +1,9 @@
+import time
 import sys
 import argparse
 import json
 import requests
-from openpyxl import load_workbook
+from openpyxl import load_workbook, Workbook
 
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -20,101 +21,129 @@ def parse_cli_args():
     args = parser.parse_args()
     # validate authentication info
     if not args.user_name or not args.password:
-        logger.info('[Abort] Missing accout name or password')
-        raise Exception
+        raise Exception('[Abort] Missing accout name or password')
     # validate existence of input excel file
     if not args.path:
-        logger.info('[Abort] Missing input file path.')
-        raise Exception
+        raise Exception('[Abort] Missing input file path.')
     return args
 
 def login(username, password):
-    logger.info('Identifying...')
     endpoint = r'https://apcndaec3ycs12.ra-int.com/raauthentication/api/user'
     payload = { 'UserName': username, 'Password': password }
     req = requests.post(endpoint, data=payload, verify=False)
-
     if req.status_code == 200:
-        logger.info('Identify successfully.')
         return req.json()['IdToken']
-    elif req.status_code == 401:
-        raise Exception('Unauthorized.')
     else:
-        raise Exception('Identify failed.')
+        raise Exception('[Abort] Unauthorized.')
+
+def get_authorization_header(id_token):
+    if not id_token:
+        raise Exception('Unauthorized.')
+    return { 'Authorization': 'Bearer ' + id_token }
 
 def query_book(isbn):
     """Query books details via books open API."""
     if not isbn or not isinstance(isbn, str):
         raise Exception
-
     endpoint = r'https://apcndaec3ycs12.ra-int.com/ralibrary/api/book/isbn/'
     query_endpoint = '{0}{1}'.format(endpoint, isbn)
     req = requests.get(query_endpoint, verify=False)
-
     if req.status_code == 200:
         return req.json()
     else:
-        raise Exception
+        raise
 
-def read_excel(file_path):
-    """Process Excel file."""
+def save_book(book, headers):
+    """Save book to RaLibrary."""
+    endpoint = r'https://apcndaec3ycs12.ra-int.com/ralibrary/api/books'
+    req = requests.post(endpoint, headers=headers, data=book, verify=False)
+    if req.status_code != 200:
+        raise Exception(req.text)
+
+def read_excel_rows(file_path):
+    """Read excel rows and skip the first header row."""
     wb = load_workbook(filename=file_path, read_only=True)
     ws = wb.active
     rows = iter(ws.rows)
-
     # skip header row
-    # next(rows)
+    next(rows)
+    return rows
 
-    for row in rows:
-        isbn = str(row[0].value)
-        code = row[1].value
-        title = row[2].value
-
-        if not isbn or not code or not title:
-            pass
+def process_row(row):
+    isbn, code, title = map(lambda cell: str(cell.value), row)
+    if not isbn or not code or not title:
+        pass
+    book = {}
+    try:
+        book = query_book(isbn)
+    except:
+        if len(isbn) == 10:
+            isbn_10 = isbn
+            isbn_13 = ''
         else:
-            try:
-                book = query_book(isbn)
-                book['Code'] = code
-                save_book(book)
-            except:
-                if len(isbn) == 10:
-                    isbn_10 = isbn
-                    isbn_13 = ''
-                else:
-                    isbn_10 = ''
-                    isbn_13 = isbn
-                save_book({
-                    'ISBN10': isbn_10,
-                    'ISBN13': isbn_13,
-                    'Code': code,
-                    'Title': title})
-
-def save_book(book):
-    """Save book to RaLibrary."""
-    endpoint = r'https://apcndaec3ycs12.ra-int.com/ralibrary/api/books'
-    headers = { 'Authorization': 'Bearer ' + id_token }
-    req = requests.post(endpoint, headers=headers, data=book, verify=False)
-
-    if req.status_code == 200:
-        logger.info('Added {0}'.format(req.json()['Title']))
-    else:
-        logger.info('Failed to add {0}'.format(book['Title']))
+            isbn_10 = ''
+            isbn_13 = isbn
+        book['ISBN10'] = isbn_10
+        book['ISBN13'] = isbn_13
+        book['Title'] = title
+    # 'Code' must be manually assigned.
+    book['Code'] = code
+    return book
 
 def main():
     # Parse command line arguments.
     try:
+        logger.info('Parsing command line parameters...')
         args = parse_cli_args()
-    except:
+        logger.info('Parsing command line parameters...Successfully')
+    except Exception as e:
+        logger.info(e);
         return 1
 
     # Authentication.
     try:
+        logger.info('Verifying your credentials...')
         id_token = login(args.user_name, args.password)
+        logger.info('Verifying your credentials...Successfully')
     except Exception as e:
-        logger.info('[Abort] {0}'.format(e))
+        logger.info(e);
         return 1
 
-    read_excel(args.path)
+    try:
+        logger.info('Reading Excel file...')
+        rows = read_excel_rows(args.path)
+        logger.info('Reading Excel file...Successfully')
+    except:
+        logger.info('[Abort] Cannot read the input Excel file.')
+        return 1
+
+    try:
+        # Create a new worksheet inside input Excel file for logging purpose.
+        wb = load_workbook(filename=args.path)
+        ws_name = 'FailedAddedBooks_{0}'.format(str(time.time())[0:10])
+        ws_log = wb.create_sheet(title=ws_name)
+        # Generate authorization HTTP header.
+        authorization_header = get_authorization_header(id_token)
+        # Start to create books to RaLibrary.
+        logger.info('Creating books to RaLibrary...')
+        failureCount = 0
+        for row in rows:
+            book = process_row(row)
+            try:
+                save_book(book, authorization_header)
+            except Exception as e:
+                failureCount += 1
+                log_row = list(map(lambda cell: str(cell.value), row))
+                log_row.append(str(e))
+                ws_log.append(log_row)
+        wb.save(filename=args.path)
+        logger.info('Creating books to RaLibrary...Done')
+        logger.info('Total failures: {0}'.format(failureCount))
+        logger.info('View the logs:')
+        logger.info('    File: {0}'.format(args.path))
+        logger.info('    Worksheet: {0}'.format(ws_name))
+    except Exception as e:
+        logger.info(e)
+        return 1
 
     return 0
